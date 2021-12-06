c63653cbfdd7bf0f58234b3b78999a180218069b
Ocawesome101 <ocawesome101@gmail.com>
2021-12-06 13:16:31 -0500

    update build script

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@ CC=i386-elf-gcc
 LD=i386-elf-ld
 
 cd bootloader
-nasm -f bin boot.asm -o ../boot.bin
+nasm -f bin boot.asm -o /tmp/boot.bin
 cd ..
 
 
 $CC -ffreestanding -c kernel/main.c -o /tmp/kernel.o
 nasm kernel/entry.asm -f elf -o /tmp/entry.o
-$LD -Ttext 0x1000 --oformat binary /tmp/entry.o /tmp/kernel.o -o kernel.bin
+$LD -Ttext 0x1000 --oformat binary /tmp/entry.o /tmp/kernel.o -o /tmp/kernel.bin
 
 
-cat boot.bin kernel.bin > osimage.bin
+cat /tmp/boot.bin /tmp/kernel.bin > osimage.bin
 
 
-qemu-system-x86_64 -fda osimage.bin
+if [ "$1" = "run" ]; then
+  qemu-system-x86_64 -fda osimage.bin
+fi
